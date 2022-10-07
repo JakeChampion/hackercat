2bbe05e7dcda595ebb5437ce4c8dc4f0b83c2f74
Jake Champion <jchampion@fastly.com>
2022-10-07 02:21:53 +0100

    compress

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 // src/index.ts
 import { Hono } from 'hono'
+// import { compress } from 'hono/compress'
 import { icon } from './handlers/icon';
 import { item } from './handlers/item';
 import { redirectToTop } from './handlers/redirectToTop';
 import { top } from './handlers/top';
 import { user } from './handlers/user';
 
+console.trace = console.log
+
+const compress = (options) => {
+  return async (ctx, next) => {
+    await next()
+    if (ctx.res.body) {
+      const encoding = ctx.req.headers.get('Accept-Encoding')?.match(options?.encoding ?? /gzip|deflate/)?.[0];
+      if (encoding) {
+        const stream = new CompressionStream(encoding)
+        ctx.res = new Response(ctx.res.body.pipeThrough(stream), ctx.res)
+        ctx.res.headers.set('Content-Encoding', encoding)
+      }
+    }
+  }
+}
+
 const app = new Hono()
+app.use('*', compress())
 app.head('/', redirectToTop)
 app.get('/', redirectToTop)
 app.head('/top', redirectToTop)
