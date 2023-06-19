514c362acb9aaf6102cde9938b6fb626dab139e5
Jake Champion <me@jakechampion.name>
2023-06-19 23:00:41 +0100

    Update deploy-to-dev-and-test.yml

--- src/index.js
+++ src/index.js
@@ -18,34 +18,18 @@ async function compress (ctx, next) {
 
 const app = new Hono()
 app.use('*', logger())
-app.use('*', compress())
-app.head('/', redirectToTop)
+app.use('*', compress)
 app.get('/', redirectToTop)
-app.head('/top', redirectToTop)
 app.get('/top', redirectToTop)
-app.head('/top/', redirectToTop)
 app.get('/top/', redirectToTop)
-app.head('/icon.svg', icon)
 app.get('/icon.svg', icon)
-app.head('/top/:pageNumber{[1]?[0-9]|20}', (c) => {
-  const pageNumber = Number.parseInt(c.req.param('pageNumber'), 10);
-  return top(pageNumber)
-})
 app.get('/top/:pageNumber{[1]?[0-9]|20}', (c) => {
   const pageNumber = Number.parseInt(c.req.param('pageNumber'), 10);
   return top(pageNumber)
 })
-app.head('/item/:id{[0-9]+}', (c) => {
-  const id = Number.parseInt(c.req.param('id'), 10);
-  return item(id)
-})
 app.get('/item/:id{[0-9]+}', (c) => {
   const id = Number.parseInt(c.req.param('id'), 10);
   return item(id)
-})
-app.head('/user/:name', (c) => {
-  const name = c.req.param('name')
-  return user(name)
 })
 app.get('/user/:name', (c) => {
   const name = c.req.param('name')
