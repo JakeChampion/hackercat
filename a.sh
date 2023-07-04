86351f1b6909127ea541ffdaf6f6e461969d6021
Jake Champion <me@jakechampion.name>
2023-07-04 11:02:13 +0100

    Delete a.sh

--- a.sh
+++
@@ -1,6 +0,0 @@
-set -eux
-if fastly service describe --quiet --service-name="hn-243" --profile personal ; then
-  FASTLY_SERVICE_ID="$(fastly service describe --service-name="hn-243" --profile personal --quiet --json | jq -r '.ID')" fastly compute publish --verbose --profile personal
-else
-  fastly compute publish --verbose -i --profile personal
-fi
