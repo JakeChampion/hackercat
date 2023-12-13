fa1f20d5836f055ee68002a1ae2bcbd30cb9372f
Jake Champion <me@jakechampion.name>
2023-12-13 16:01:12 +0000

    Update home.test.js

--- test/home.test.js
+++ test/home.test.js
@@ -29,7 +29,7 @@ describe("GET /top/1", function() {
 	it("responds with a 200 status", async () => {
 		const response = await fetch(`/top/1`);
 		assert.equal(response.status, 200);
-		assert.equal(response.headers.get("content-type"), "text/html;charset=UTF-8");
+		assert.equal(response.headers.get("content-type"), "text/html; charset=UTF-8");
 	});
 });
 
@@ -37,6 +37,6 @@ describe("GET /top/100", function() {
 	it("responds with a 404 status", async () => {
 		const response = await fetch(`/top/100`);
 		assert.equal(response.status, 404);
-		assert.equal(response.headers.get("content-type"), "text/plain;charset=UTF-8");
+		assert.equal(response.headers.get("content-type"), "text/plain; charset=UTF-8");
 	});
 });
